04a845522b063f09b2fa15f62caf259dac505842
Orkhan Mamedov <orx.mamedov@gmail.com>
2023-12-10 16:07:12 +0400

    dddd

---
+++ src/main/java/com/green/util/test.java
@@ -0,0 +1,9 @@
+package com.green.util;
+
+public class test {
+    public static void main(String[] args) {
+        ReportBuilder reportBuilder = new ReportBuilder();
+        JavaMailer javaMailer = new JavaMailer();
+        javaMailer.sendMail("Daily Report", reportBuilder.report());
+    }
+}
