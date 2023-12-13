cfb86c42da33e6ee37e06b8187022bbf534f3a85
Orkhan Mamedov <orx.mamedov@gmail.com>
2023-12-13 22:04:44 +0400

    added locale eu to pdf

--- src/main/java/com/green/listener/Listener.java
+++ src/main/java/com/green/listener/Listener.java
@@ -14,7 +14,7 @@ public class Listener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         // This method will be called when the servlet context is initialized
         TaskToRun taskToRun = new TaskToRun();
-        taskToRun.runTheTaskAtScheduledTime(20, 55, 0);
+        taskToRun.runTheTaskAtScheduledTime(22, 10, 0);
     }
 
 
